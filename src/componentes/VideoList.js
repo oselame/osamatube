7258080188d2c4aba1af7587691bec89ed8f3588
Adriano Oselame <adrianooselame@gmail.com>
2019-01-27 17:36:28 -0200

    Iniciando o projeto, criando componentes

---
+++ src/componentes/VideoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoList = props => {
+    return (
+        <div><h1>Video List</h1></div>
+    );
+}
+
+export default VideoList;
---
+++ src/componentes/VideoPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoPlayer = props => {
+    return (
+        <div><h1>Video Player</h1></div>
+    );
+}
+
+export default VideoPlayer;
