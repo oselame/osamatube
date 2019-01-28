8917688f812d56991bf535cc567a244a43ab6907
Adriano Oselame <adrianooselame@gmail.com>
2019-01-27 23:18:37 -0200

    Iniciando o Video Player

--- src/componentes/VideoPlayer.js
+++ src/componentes/VideoPlayer.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { Advertisement } from 'semantic-ui-react';
 
 const VideoPlayer = props => {
     return (
         <div className="video-player">
-            <h1>Video Player</h1>
+            <div>
+                <p>{JSON.stringify(props)}</p>
+            </div>
+            <Advertisement style={{ 'height': '433px' }} unit='top banner' test="Escolha um vídeo para reproduzir">   
+             </Advertisement>
         </div>
     );
 }
 
-export default VideoPlayer;
+const mapStateToProps = (state) => {
+    return {
+        video: state.reproduzVideoReducer.video
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoPlayer);
