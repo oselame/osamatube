fac8d4de818ec6d8c33900796add844002f582bb
Adriano Oselame <adrianooselame@gmail.com>
2019-01-27 23:24:30 -0200

    Reproduzindo o video do youtube

--- src/componentes/VideoPlayer.js
+++ src/componentes/VideoPlayer.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Advertisement } from 'semantic-ui-react';
+import { Advertisement, Embed } from 'semantic-ui-react';
 
 const VideoPlayer = props => {
     return (
         <div className="video-player">
-            <div>
-                <p>{JSON.stringify(props)}</p>
-            </div>
-            <Advertisement style={{ 'height': '433px' }} unit='top banner' test="Escolha um vídeo para reproduzir">   
-             </Advertisement>
+            {
+                !props.video.id && (
+                    <Advertisement style={{ 'height': '433px' }} unit='top banner' test="Escolha um vídeo para reproduzir" />
+                )
+            }
+            {
+                props.video.id && (
+                    <div>
+                        <Embed id={props.video.id.videoId} source='youtube'/>
+                        <p>{props.video.snippet.title}</p>
+                        <p>{props.video.snippet.description}</p>
+                    </div>
+                )
+            }
+
         </div>
     );
 }
